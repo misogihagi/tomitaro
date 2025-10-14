5aa152f13c24f220a57e77deda599651450bb9c4
misogihagi <47350059+misogihagi@users.noreply.github.com>
2025-10-14 22:18:14 +0900

    Update main.py

--- mimamori/main.py
+++ mimamori/main.py
@@ -1,19 +1,6 @@
 import time
-from cli import get_and_save_data
+from cli import main
 import schedule
 
 if __name__ == "__main__":
-    # スケジュール設定
-    # 15分ごとに get_and_save_data 関数を実行するように設定
-    schedule.every(15).minutes.do(get_and_save_data)
-    
-    print("\n==============================================")
-    print("データ取得スケジューラーが起動しました。")
-    print("15分ごとにデータ取得とSQLite保存を実行します。")
-    print("停止するには Ctrl+C を押してください。")
-    print("==============================================")
-    
-    # プログラムが終了しないように無限ループでスケジュールをチェック
-    while True:
-        schedule.run_pending()
-        time.sleep(1) # スケジュールのチェック間隔 (1秒ごと)
+    main()
