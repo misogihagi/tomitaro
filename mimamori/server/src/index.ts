80faff28e9a03e75559945fb5d1c6dec86f78795
misogihagi <47350059+misogihagi@users.noreply.github.com>
2026-03-28 01:53:25 +0900

    create elysia src/index.ts

--- mimamori/server/src/index.ts
+++ mimamori/server/src/index.ts
@@ -1,9 +1,17 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from "elysia";
+import { createInsertSchema } from "drizzle-typebox";
+import { measurements } from "./db/schema";
+
+const _createMeasurement = createInsertSchema(measurements, {
+  timestamp: t.Date(),
+});
 
 const app = new Elysia()
-	.get('/', () => 'Hello Elysia')
-	.listen(3000)
+  .post("/", ({ body }) => console.log, { body: _createMeasurement })
+  .listen(3000);
 
 console.log(
-	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-)
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);
+
+export type App = typeof app;
