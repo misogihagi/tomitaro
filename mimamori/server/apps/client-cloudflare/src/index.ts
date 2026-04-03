484ff0366d81697110a91c6970d3812ae8400d2d
misogihagi <47350059+misogihagi@users.noreply.github.com>
2026-04-04 00:29:43 +0900

    add client cloudflare example

---
+++ mimamori/server/apps/client-cloudflare/src/index.ts
@@ -0,0 +1,22 @@
+import { treaty } from '@elysiajs/eden'
+import type { App } from '@repo/server'
+
+const app = treaty<App>('localhost:3000', {
+    headers: {
+        'Authorization': 'Bearer xxxxxxxxxx'
+    }
+})
+
+const testMeasurement = {
+    timestamp: '2026-03-28T10:00:00Z',
+    site: 'Greenhouse-A',
+    temperature: 24.5,
+    humidness: 65.2,
+    EC_conductivity: 1.2,
+    PH: 6.5,
+    Nitrogen: 150.5,
+    Phosphorus: 45.0,
+    Potassium: 210.2,
+}
+
+console.log(await app.post(testMeasurement))
